733e77d5f1bb5cfc764e95f854d5896dab5fd3b1
jehkinen <jehkinen>
2013-02-22 00:28:37 +0600

    тестовый

--- application/views/register/index.blade.php
+++ application/views/register/index.blade.php
@@ -1,7 +1,7 @@
 @layout('master')
 
 @section('content')
-    <h1>register.index</h1>
+    <h1>Регистрация пользователей</h1>
 
     <p>This view has been auto-generated to accompany the Register_Controller's action_index()</p>
 @endsection
