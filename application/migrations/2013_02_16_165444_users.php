65db9bd0814025dd969ac888731d4bd216f56dd1
jehkinen <jehkinen>
2013-02-23 17:16:02 +0600

    merge with master

--- application/migrations/2013_02_16_165444_users.php
+++ application/migrations/2013_02_16_165444_users.php
@@ -14,7 +14,9 @@ class Users {
 			$table->timestamps();
 			$table->string('phone', 11);
 			$table->string('password');
-			$table->string('name')->nullable();
+            $table->string('name_and_surname')->nullable();
+            $table->drop_column('name');
+            $table->drop_column('surname');
 			$table->text('about')->nullable();
 			$table->integer('status')->default(0);  //это статус 0 неподтвержденный, 1- незаполненный, 2 - уже в поиске
 			$table->boolean('is_worker')->default(true); //true -рабочий, false - работодат.
